d3b3a45a75c8c0b923277ea3ec0db6c13e8eef0f
AniaTofik <anna.kiedel@onet.pl>
2022-10-15 14:11:59 +0200

    feat: Map Response to Model

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,13 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs";
-import {PersonModel} from "../model/person.model";
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { PersonModel } from '../model/person.model';
+import { ApiResponse } from './api.response';
+import { CreateEmployeeModel } from '../model/create-employee.model';
+import {EmployeeResponse} from "./employee.response";
 
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
-  getAll() : Observable<PersonModel[]>{
-    return this._httpClient.get<PersonModel[]>( '/assets/data/people.json');
+  getAll(): Observable<PersonModel[]> {
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>(
+      'https://dummy.restapiexample.com/api/v1/employees',
+      ).pipe(
+        map((response: ApiResponse<EmployeeResponse[]>) => {
+          return response.data.map((employeeResponse: EmployeeResponse) => {
+            return {
+              name: employeeResponse.employee_name,
+              personalNumber: employeeResponse.id,
+              img:employeeResponse.profile_image,
+              surname: '',
+              mail: '',
+            }
+          });
+        })
+      )
+        }
+
+  create(employee: CreateEmployeeModel): Observable<any> {
+    return this._httpClient.post('https://dummy.restapiexample.com/create', employee).pipe(map((arrayBuffer): any => undefined));
+  }
+
+  delete(id: string): Observable<void> {
+    return this._httpClient.delete('https://dummy.restapiexample.com/delete' + id).pipe(map(_ => void 0));
+
   }
 }
--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import { CreateEmployeeModel } from '../../model/create-employee.model';
 
 @Component({
   selector: 'app-employee-form',
@@ -12,13 +14,19 @@ export class EmployeeFormComponent {
     {
       name: new FormControl('', Validators.required),
       age: new FormControl('', Validators.required),
-      salary: new FormControl('', Validators.required)
+      salary: new FormControl('', Validators.required),
     });
 
-  onEmployeeFormSubmitted(employeeForm: FormGroup): void {
+  constructor(private _employeeService: EmployeeService) {
   }
-  onButtonClicked(form: {name: string, age: string, salary: string}){
+
+  onButtonClicked(form: { name: string, age: string, salary: string }) {
     alert(`User was successfully added to the database. Name: ${form.name}, age: ${form.age}, salary: ${form.salary}$.`);
+  }
+  onEmployeeFormSubmitted(form: CreateEmployeeModel) {
+    this._employeeService.create(form).subscribe((form:any)=>{
+      alert('sukces');
+    });
   }
 
 }
